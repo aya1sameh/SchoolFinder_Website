8021cf90827b4a09f8dbfb49044a5f78acec62b4
AyaSameh <aya.sameh.99@gmail.com>
2020-10-05 23:47:54 +0200

    Signup designs updated/adding login page

---
+++ schoolfinder_web/src/views/Login.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-app>
+    <v-content id="content">
+      <v-container id="container" class="mt-10 mb-10">
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    };
+  },
+  methods: {
+  },
+};
+</script>
+<style scoped>
+#content {
+  background-color: #008080;
+}
+#container {
+  background-color: #eeeeee;
+  border-radius: 50px;
+  width: 80%;
+}
+</style>>
--- schoolfinder_web/src/views/SignUp.vue
+++ schoolfinder_web/src/views/SignUp.vue
@@ -12,21 +12,37 @@
           </v-col>
         </v-row>
 
+        <v-form ref="form">
         <v-row justify="center" class="mb-5">
-          <v-col cols="3" sm="2" md="1" id="avatar">
-            <v-avatar >
-              <v-icon class="mdi-48px">mdi-account</v-icon>
-            </v-avatar>
+          <v-col offset="1" cols="3" sm="4" md="2" id="avatar">
+            <v-img
+              src="../assets/SignupAvatar.png"
+              max-height="100"
+              max-width="100"
+            >
+            </v-img>
+          </v-col>
+          <v-col cols="1" sm="1" md="1">
+            <br />
+            <br />
+            <br />
+            <v-file-input
+              hide-input
+              accept="image/*"
+              color="#009688"
+              prepend-icon="mdi-camera-plus"
+            ></v-file-input>
           </v-col>
         </v-row>
 
         <v-row justify="center" no-gutters>
           <v-col cols="12" sm="6" md="5">
             <v-text-field
+              v-model="Name"
               label="Name"
               prepend-inner-icon="mdi-account"
               color="#009688"
-              :rules="[rules.required]"
+              :rules="[rules.required, rules.Namelength]"
               outlined
             ></v-text-field>
           </v-col>
@@ -35,10 +51,11 @@
         <v-row justify="center" no-gutters>
           <v-col cols="12" sm="6" md="5">
             <v-text-field
+              v-model="Email"
               label="Email"
               prepend-inner-icon="mdi-email"
               color="#009688"
-              :rules="[rules.required]"
+              :rules="[rules.required, rules.email]"
               outlined
             ></v-text-field>
           </v-col>
@@ -47,12 +64,13 @@
         <v-row justify="center" no-gutters>
           <v-col cols="12" sm="6" md="5">
             <v-text-field
+              v-model="password"
               :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
               :type="show1 ? 'text' : 'password'"
               label="Password"
               prepend-inner-icon="mdi-lock"
               color="#009688"
-              :rules="[rules.required]"
+              :rules="[rules.required, rules.Passlength]"
               outlined
               @click:append="show1 = !show1"
             ></v-text-field>
@@ -62,12 +80,13 @@
         <v-row justify="center" no-gutters>
           <v-col cols="12" sm="6" md="5">
             <v-text-field
+              v-model="Confirmpassword"
               :append-icon="show2 ? 'mdi-eye' : 'mdi-eye-off'"
               :type="show2 ? 'text' : 'password'"
               label="Confirm Password"
               prepend-inner-icon="mdi-lock"
               color="#009688"
-              :rules="[rules.required]"
+              :rules="[rules.required, rules.Matchingchar(Confirmpassword, password)]"
               outlined
               @click:append="show2 = !show2"
             ></v-text-field>
@@ -77,6 +96,7 @@
         <v-row justify="center" no-gutters>
           <v-col cols="12" sm="6" md="5">
             <v-text-field
+              v-model="PhoneNumber"
               label="Phone Number"
               prepend-inner-icon="mdi-phone"
               color="#009688"
@@ -89,6 +109,7 @@
         <v-row justify="center" no-gutters>
           <v-col cols="12" sm="6" md="5">
             <v-text-field
+              v-model="Location"
               label="Location"
               prepend-inner-icon="mdi-map-marker"
               color="#009688"
@@ -124,6 +145,7 @@
         <v-row justify="center" no-gutters>
           <v-col cols="12" sm="6" md="5">
             <v-select
+              v-model="SchoolSelected"
               v-show="role == 2"
               :items="items"
               label="Select A School"
@@ -131,20 +153,15 @@
               dense
               outlined
             >
-              <v-slider
-                v-model="duration"
-                min="0"
-                max="1000"
-                label="Duration"
-                thumb-label
-              ></v-slider>
             </v-select>
           </v-col>
         </v-row>
+
+        </v-form>
         <br />
         <v-row justify="center" no-gutters>
           <v-col cols="24" sm="18" md="15">
-            <v-btn rounded dark color="#009688" x-large style="width: 250px">
+            <v-btn @click="Validate" rounded dark color="#009688" x-large style="width: 250px">
               Register
             </v-btn>
           </v-col>
@@ -152,8 +169,15 @@
         <br />
         <v-row justify="center" no-gutters>
           <v-col cols="24" sm="18" md="15">
-            <v-btn outlined rounded color="#009688" large style="width: 200px">
-              Login
+            <v-btn
+              to='/login'
+              outlined
+              rounded
+              color="#009688"
+              large
+              style="width: 200px"
+            >
+            Login
             </v-btn>
           </v-col>
         </v-row>
@@ -168,10 +192,24 @@ export default {
     return {
       show1: false,
       show2: false,
-      role: 1,
+      role: 0,
+      Name: '',
+      Email: '',
+      password: '',
+      Confirmpassword: '',
+      PhoneNumber: '',
+      Location: '',
+      SchoolSelected: '',
       items: ['school_1', 'school_2', 'school_3', 'school_4'],
       rules: {
         required: (value) => !!value || 'Required.',
+        email: (value) => {
+          const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+          return pattern.test(value) || 'Invalid e-mail.';
+        },
+        Passlength: (value) => (value.length >= 8) || 'Min 8 characters',
+        Namelength: (value) => (value.length >= 3 && value.length <= 64) || 'Min 3 characters and Max 64 characters',
+        Matchingchar: (Confirmpassword, password) => this.IsMatching(Confirmpassword, password) || 'Confirm password must be the same as the password',
         number: (value) => this.IsaNumber(value) || 'Not a Valid Number',
       },
     };
@@ -179,8 +217,19 @@ export default {
   methods: {
     IsaNumber(value) {
       const phoneno = /^\d{11}$/;
-      if (value.match(phoneno)) return true;
+      if (value.match(phoneno)) {
+        return true;
+      }
       return false;
+    },
+    IsMatching(Confirmpassword, password) {
+      if (Confirmpassword !== password) return false;
+      return true;
+    },
+    Validate() {
+      if (this.$refs.form.validate()) {
+        this.$router.push('/');
+      }
     },
   },
 };
@@ -202,8 +251,13 @@ export default {
   color: #009688;
 }
 #avatar {
-  border-radius: 60%;
-  border: solid 1px #009688;
-  text-align: center;
+  border-radius: 50%;
+  border: solid 2px #009688;
+  max-width: 200px;
+  max-height: 200px;
+  display:flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
 }
 </style>>
