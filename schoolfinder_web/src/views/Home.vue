23301ae8a609e9a3e20a56e383851e634303962b
Ranime1999 <Ranime.mohamed99@eng-st.cu.edu.eg>
2020-10-14 06:38:45 +0200

    ads pictures applied

--- schoolfinder_web/src/views/Home.vue
+++ schoolfinder_web/src/views/Home.vue
@@ -5,7 +5,7 @@
     <v-carousel-item
       v-for="(item,i) in items"
       :key="i"
-      :src="item.src"
+      :src=base_url+item.ad_image_url
     ></v-carousel-item>
   </v-carousel>
   </v-app>
@@ -13,6 +13,7 @@
 
 <script>
 // @ is an alias to /src
+import axios from 'axios';
 import Header from '@/components/Header.vue';
 
 export default {
@@ -21,20 +22,19 @@ export default {
     Header,
   },
   data: () => ({
-    items: [
-      {
-        src: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg',
-      },
-      {
-        src: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg',
-      },
-      {
-        src: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg',
-      },
-      {
-        src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg',
-      },
-    ],
+    base_url: 'http://127.0.0.1:8000/',
+    items: {},
   }),
+  methods: {
+    getAds() {
+      axios.get('http://127.0.0.1:8000/api/ads', { headers: { APP_KEY: 'c2Nob29sX2ZpbmRlcl9hcHBfa2V5ZmJkamhqeGNoa2N2anhqY2p2Ymh4amM6dmFzZGhoYXNkaGphZHNrZHNmYW1jbmhkc3VoZHVoY3Nq' } })
+        .then((response) => {
+          this.items = response.data;
+        });
+    },
+  },
+  created() {
+    this.getAds();
+  },
 };
 </script>
